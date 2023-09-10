cc655c7907eb04ded7984e5414590e6243ec093a
laila-rin <56868999+laila-rin@users.noreply.github.com>
2023-09-10 13:29:21 +1000

    Tipps mapping

--- src/pages/FinalPage.tsx
+++ src/pages/FinalPage.tsx
@@ -11,8 +11,56 @@ export const FinalPage = (props: PageProps) => {
 
   const tipps: string[] = [];
 
-  if (props.isAnswerSelected(questions["Welche Suchmaschine(n) benutzt du?"]["Google"])) {
-    tipps.push("Haha schlecht.")
+  if (props.isAnswerSelected(questions["Welche Suchmaschine(n) benutzt du?"]["Google"]) || props.isAnswerSelected(questions["Welche Suchmaschine(n) benutzt du?"]["Bing"] || props.isAnswerSelected(questions["Welche Suchmaschine(n) benutzt du?"]["Keine Ahnung"]))) {
+    tipps.push("Es ist eine gute Idee, Suchmaschinen wie Startpage oder DuckDuckGo zu verwenden, da sie weniger persönliche Informationen speichern und sicherer für deine Online-Suchen sind.")
+  }
+
+  if (props.isAnswerSelected(questions["Wie verschickst du Nachrichten?"]["WhatsApp"]) || props.isAnswerSelected(questions["Wie verschickst du Nachrichten?"]["TikTok"]) || props.isAnswerSelected(questions["Wie verschickst du Nachrichten?"]["Snapchat"]) || props.isAnswerSelected(questions["Wie verschickst du Nachrichten?"]["Instagram"]) || props.isAnswerSelected(questions["Wie verschickst du Nachrichten?"]["BeReal"])) {
+    tipps.push("Denke daran, dass Nachrichten auf sozialen Plattformen nicht immer privat sind. Es ist deshalb besser Messenger-Apps zu verwenden, die Ende-zu-Ende-Verschlüsselung anbieten, wie Signal oder Threema.")
+  }
+
+  if (props.isAnswerSelected(questions["Wie verschickst du Nachrichten?"]["WhatsApp"])) {
+    tipps.push("Viele Messenger Apps speichern nicht nur deine Daten, wie Nachrichten und Bilder, sondern haben sogar ein Recht darauf, sie weiter zu benutzen! Das heisst das peinliche Bild, das du deiner Besten Freundin oder deinem besten Freund auf WhatsApp geschickt hast, das koennte WhatsApp fuer Plakate nuten oder anderweitig verteilen und du koenntest nichts dagegen tun. Manche Firmen haben die Nutzung von WhatsApp fuer Ihre Mitarbeiter deshalb sogar verboten. Und wusstest du, dass das Mindestalter zur Nuztung von WhatsApp mittlerweile bei 16 Jahren ist?")
+  }
+
+  if (props.isAnswerSelected(questions["Wie häufig löscht du deinen Browser-Cache?"]["Was ist das?"])) {
+    tipps.push("Der Browser-Cache speichert Daten von besuchten Websites. Es ist ratsam, ihn gelegentlich zu löschen, um Speicherplatz freizugeben und deine Privatsphäre zu schützen. Du kannst dies in den Browsereinstellungen tun.")
+  }
+
+  if (props.isAnswerSelected(questions["Hast du die Ortungsfunktion in deinem Handy eingeschalten?"]["Ja"]) || props.isAnswerSelected(questions["Hast du die Ortungsfunktion in deinem Handy eingeschalten?"]["Sowas gibt es?"])) {
+    tipps.push("Die Ortungsfunktion in deinem Handy kann nützlich sein, um dein Handy zu finden wenn du es mal verlegt hast - aber es ist wichtig, sie nicht immer eingeschaltet zu lassen, um deine Privatsphäre zu schützen. Du solltest sie nur bei Bedarf aktivieren.")
+  }
+
+  if (props.isAnswerSelected(questions["Du gehst auf eine Webseite und wirst nach Cookies gefragt - was machst du?"]["Akzeptieren und schnell weiter."]) || props.isAnswerSelected(questions["Du gehst auf eine Webseite und wirst nach Cookies gefragt - was machst du?"]["Falls ein 'nur essenzielle Cookies' angezeigt wird, dann nehme ich den. Sonst einfach akzeptieren."])) {
+    tipps.push("Es ist klug, Cookies auf Webseiten bewusst zu verwalten, um deine Privatsphäre zu schützen. Wähle nur die essenziellen Cookies aus, die du wirklich brauchst, und lehne nicht notwendige Cookies ab.")
+  }
+
+  if (props.isAnswerSelected(questions["Teilst du deinen Standort auf der Snapchat Map?"]["Immer"]) || props.isAnswerSelected(questions["Teilst du deinen Standort auf der Snapchat Map?"]["Manchmal"]) || props.isAnswerSelected(questions["Verlinkst du deinen Standort auf Instagram Posts?"]["Immer"]) || props.isAnswerSelected(questions["Verlinkst du deinen Standort auf Instagram Posts?"]["Manchmal, wenn ich an coolen Orten bin"])) {
+    tipps.push("Es ist verlockend, deine Standorte auf Instagram oder in anderen sozialen Medien zu teilen, aber denke daran, dass es gefährlich sein kann, dein Zuhause oder andere persönliche Orte öffentlich preiszugeben. Schütze deine Privatsphäre, indem du vermeidest, sensiblen Orten eine genaue Markierung zu geben, um deine Sicherheit und die deiner Familie zu gewährleisten.")
+  }
+
+  if (props.isAnswerSelected(questions["Teilst du deinen Standort auf der Snapchat Map?"]["Immer"]) || props.isAnswerSelected(questions["Benutzt du deinen echten Namen in sozialen Medien?"]["Manchmal"]) || props.isAnswerSelected(questions["Benutzt du deinen echten Namen in sozialen Medien?"]["Klar, bin ja ich"])) {
+    tipps.push("Die Verwendung deines echten Namens in deinen online Profilen kann es leichter machen, dich online zu finden - das gilt aber leider nicht nur für deine Freunde, sondern auch für Fremde. Verwende vielleicht nur deinen Vornamen, um dich und deine Identität zu schützen.")
+  }
+
+  if (props.isAnswerSelected(questions["Sei ehrlich - benutzt du überall das gleiche Passwort?"]["Ja, immer das gleiche."]) || props.isAnswerSelected(questions["Sei ehrlich - benutzt du überall das gleiche Passwort?"]["Ich denke mir immer neue aus."]) || props.isAnswerSelected(questions["Sei ehrlich - benutzt du überall das gleiche Passwort?"]["Ich habe ein paar verschiedene."])) {
+    tipps.push("Es ist besser, immer ein einzigartiges Passwort zu verwenden. Das hilft, deine Accounts sicherer zu machen. Du könntest einen Passwortmanager verwenden, um dir dabei zu helfen, alle Passwörter sicher zu organisieren.")
+  }
+
+  if (props.isAnswerSelected(questions["Wie viele der Personen, die dir auf Instagram, Snapchat und anderen Medien folgen, kennst du persönlich?"]["Fast niemanden"]) || props.isAnswerSelected(questions["Wie viele der Personen, die dir auf Instagram, Snapchat und anderen Medien folgen, kennst du persönlich?"]["Die meisten"])) {
+    tipps.push("Es ist wichtig, vorsichtig zu sein und nur Personen auf Instagram, Snapchat und anderen sozialen Medien zu akzeptieren, die du wirklich kennst. Nicht alle, die dir folgen, haben gute Absichten. Schütze deine Online-Privatsphäre, indem du nur echte Freunde und Bekannte in deinen sozialen Netzwerken akzeptierst.")
+  }
+
+  if (props.isAnswerSelected(questions["Benutzt du Smart-Home-Geräte, wie zum Beispiel Alexa?"]["Ja"])) {
+    tipps.push("Klar, Smart Home Geraete wie Alexa sind praktisch. Aber die hören oft mehr mit, als man denkt!Wenn du über private Sachen redest, denk daran, sie ab und zu auszuschalten. Das schützt deine Privatsphäre.")
+  }
+
+  if (props.isAnswerSelected(questions["Postest du Bilder von dir selbst oder deinen Freunden, auf denen man dein/eure Gesichter sieht?"]["Ja"])) {
+    tipps.push("Natürlich ist es toll, Erinnerungen und Bilder zu teilen! Aber sei vorsichtig. Stell sicher, dass du deine Kontoeinstellungen so einstellst, dass nur enge Freunde deine Fotos sehen können. So hältst du deine Bilder sicher und teilst sie nur mit den Menschen, die dir wichtig sind.")
+  }
+
+  if (props.isAnswerSelected(questions["Klickst du auf die Werbung, die dir auf Instagram, Tiktok oder anderen sozialen Medien angezeigt wird?"]["Manchmal"]) || props.isAnswerSelected(questions["Klickst du auf die Werbung, die dir auf Instagram, Tiktok oder anderen sozialen Medien angezeigt wird?"]["Oft"])) {
+    tipps.push("Manchmal können Werbeanzeigen (auch auf Instagram, Tiktok oder Snapchat) gefälscht sein oder auf unsichere Seiten führen. Denk daran, aufmerksam zu bleiben und nur auf Anzeigen zu klicken, wenn du sicher bist, dass sie echt sind. So schützt du dich vor möglichen Betrügereien oder unerwünschten Folgen.")
   }
 
   return <div className={"flex flex-col pt-20"}>
